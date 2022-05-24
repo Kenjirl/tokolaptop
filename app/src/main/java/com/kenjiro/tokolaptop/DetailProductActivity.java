1fd13833d22c445c8b0979acaa55f652f7ad6087
Benedict Kenjiro <kenjiro442@gmail.com>
2022-05-24 19:57:09 +0800

    Implement Implicit Intent on Detail Activity with Fragment

--- app/src/main/java/com/kenjiro/tokolaptop/DetailProductActivity.java
+++ app/src/main/java/com/kenjiro/tokolaptop/DetailProductActivity.java
@@ -1,27 +1,36 @@
 package com.kenjiro.tokolaptop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicBoolean;
+//import java.util.concurrent.atomic.AtomicBoolean;
 
 public class DetailProductActivity extends AppCompatActivity {
 
     ImageView btnBack;
-    ImageView icFav;
+//    ImageView icFav;
+    TextView deskripsiFgBtn, tokoFgBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_detail_product);
+
+        FragmentManager FM = getSupportFragmentManager();
+        FragmentTransaction FT = FM.beginTransaction();
+        FT.replace(R.id.fragmentLayout,new DeskripsiFragment());
+        FT.commit();
 
         btnBack = findViewById(R.id.back);
         btnBack.setOnClickListener(view -> {
@@ -30,23 +39,40 @@ public class DetailProductActivity extends AppCompatActivity {
             finish();
         });
 
-        AtomicBoolean colorRed = new AtomicBoolean(false);
+//        AtomicBoolean colorRed = new AtomicBoolean(false);
+//
+//        icFav = (ImageView) findViewById(R.id.icFav);
+//        icFav.setOnClickListener(view -> {
+//            if(!colorRed.get()){
+//                icFav.setColorFilter(getResources().getColor(R.color.red));
+//                Toast.makeText(DetailProductActivity.this, "Ditambahkan ke Favorite", Toast.LENGTH_SHORT).show();
+//                colorRed.set(true);
+//            }else {
+//                icFav.setColorFilter(getResources().getColor(R.color.black));
+//                Toast.makeText(DetailProductActivity.this, "Dihapus dari Favorite", Toast.LENGTH_SHORT).show();
+//                colorRed.set(false);
+//            }
+//        });
 
-        icFav = (ImageView) findViewById(R.id.icFav);
-        icFav.setOnClickListener(view -> {
-            if(!colorRed.get()){
-                icFav.setColorFilter(getResources().getColor(R.color.red));
-                Toast.makeText(DetailProductActivity.this, "Ditambahkan ke Favorite", Toast.LENGTH_SHORT).show();
-                colorRed.set(true);
-            }else {
-                icFav.setColorFilter(getResources().getColor(R.color.black));
-                Toast.makeText(DetailProductActivity.this, "Dihapus dari Favorite", Toast.LENGTH_SHORT).show();
-                colorRed.set(false);
-            }
-        });
+        deskripsiFgBtn = findViewById(R.id.fragmentDeskripsiBtn);
+        tokoFgBtn = findViewById(R.id.fragmentTokoBtn);
+
+        deskripsiFgBtn.setOnClickListener(view -> replaceFragment(new DeskripsiFragment()));
+
+        tokoFgBtn.setOnClickListener(view -> replaceFragment(new TokoFragment()));
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager FM = getSupportFragmentManager();
+        FragmentTransaction FT = FM.beginTransaction();
+        FT.replace(R.id.fragmentLayout,fragment);
+        FT.commit();
     }
 
     public void addToCart(View view) {
         Toast.makeText(DetailProductActivity.this, "Ditambahkan ke Keranjang", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(DetailProductActivity.this, KeranjangActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/kenjiro/tokolaptop/TokoFragment.java
@@ -0,0 +1,72 @@
+package com.kenjiro.tokolaptop;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+//import android.widget.ImageView;
+
+public class TokoFragment extends Fragment {
+
+    public TokoFragment(){}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_toko, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Giving the same onClickListener method to every image on Fragment.
+        // Each id will have different action, can be seen on doImplicit method.
+        for (int iImage=1; iImage<3; iImage++){
+            int idImage = getResources().getIdentifier("Implicit_" + iImage, "id", getContext().getPackageName());
+            View eventImage = view.findViewById(idImage);
+            eventImage.setOnClickListener(this::doImplicit);
+        }
+    }
+
+    public void doImplicit(View view){
+        // Switch case using the id from for loop on onViewCreated method.
+        switch (view.getId()){
+            // If we click on the ImageView with id = Implicit_1
+            case R.id.Implicit_1:
+                String loc = "Laptop Store Denpasar";
+
+                // Parse the location and create the intent.
+                Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, addressUri);
+
+                // Find an activity to handle the intent, and start that activity.
+                startActivity(intent1);
+                break;
+            // If we click on the ImageView with id = Implicit_2
+            case R.id.Implicit_2:
+                String number = "081246007474";
+
+                // Parse the number and create the intent.
+                Uri numberUri = Uri.parse("tel:" + number);
+                Intent intent2 = new Intent(Intent.ACTION_DIAL, numberUri);
+
+                // Find an activity to handle the intent, and start that activity.
+                startActivity(intent2);
+                break;
+        }
+    }
+}
